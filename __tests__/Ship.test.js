479633a4c52e5039690e8c1796ebbea4ce20ff74
breakfastmeansbreakfast <77744c@gmail.com>
2018-07-08 22:11:05 +0100

    DRYing up the tests

--- __tests__/Ship.test.js
+++ __tests__/Ship.test.js
@@ -4,74 +4,59 @@ const Port = require('../src/Port.js');
 const Itinerary = require('../src/Itinerary');
 
 describe('Ship', () => {
-  it('can be instantiated', () => {
-    const port = new Port('Turdington');
-    const itinerary = new Itinerary([port]);
-    const ship = new Ship(itinerary);
-    expect(ship).toBeInstanceOf(Object);
-  });
-  it('starting port', () => {
-    const port = new Port('Poopyville');
-    const itinerary = new Itinerary([port]);
-    const ship = new Ship(itinerary);
+  describe('with ports and an itinerary', () => {
+    let ship;
+    let poopyville;
+    let fartington;
+    let itinerary;
 
-    expect(ship.currentPort).toBe(port);
-  });
-  it('can set sail', () => {
-    const poopyville = new Port('Poopyville');
-    const fartington = new Port('Fartington');
-    const itinerary = new Itinerary([poopyville, fartington]);
-    const ship = new Ship(itinerary);
-    ship.setSail();
-    expect(poopyville.ships).not.toContain(ship);
-  });
-  it('can dock at another port', () => {
-    const poopyville = new Port('Poopyville');
-    const excrementville = new Port('Excrementville');
-    const itinerary = new Itinerary([poopyville, excrementville]);
-    const ship = new Ship(itinerary);
+    beforeEach(() => {
+      poopyville = new Port('Poopyville');
+      fartington = new Port('Fartington');
+      itinerary = new Itinerary([poopyville, fartington]);
+      ship = new Ship(itinerary);
+    });
 
-    ship.setSail();
-    ship.dock();
+    it('can be instantiated', () => {
+      expect(ship).toBeInstanceOf(Object);
+    });
+    it('starting port', () => {
+      expect(ship.currentPort).toBe(poopyville);
+    });
+    it('can set sail', () => {
+      ship.setSail();
+      expect(poopyville.ships).not.toContain(ship);
+    });
+    it('gets added to the port on instantiation', () => {
+      expect(poopyville.ships).toContain(ship);
+    });
+    it('can dock at another port', () => {
+      ship.setSail();
+      ship.dock();
 
-    expect(ship.currentPort).toBe(excrementville);
-    expect(excrementville.ships).toContain(ship);
-  });
-  it('can\'t sail further than its itinerary', () => {
-    const poopyville = new Port('Poopyville');
-    const excrementville = new Port('Excrementville');
-    const itinerary = new Itinerary([poopyville, excrementville]);
-    const ship = new Ship(itinerary);
+      expect(ship.currentPort).toBe(fartington);
+      expect(fartington.ships).toContain(ship);
+    });
+    it('can\'t sail further than its itinerary', () => {
+      ship.setSail();
+      ship.dock();
 
-    ship.setSail();
-    ship.dock();
+      expect(() => ship.setSail()).toThrowError('End of itinerary reached');
+    });
+    it('can add a ship', () => {
+      poopyville.addShip(ship);
 
-    expect(() => ship.setSail()).toThrowError('End of itinerary reached');
-  });
-  it('can add a ship', () => {
-    const port = new Port('Brownton');
-    const ship = {};
+      expect(poopyville.ships).toContain(ship);
+    });
+    it('can remove a ship', () => {
+      const titanic = {};
+      const mary = {};
 
-    port.addShip(ship);
+      fartington.addShip(titanic);
+      fartington.addShip(mary);
+      fartington.removeShip(mary);
 
-    expect(port.ships).toContain(ship);
-  });
-  it('can remove a ship', () => {
-    const port = new Port('Log');
-    const titanic = {};
-    const mary = {};
-
-    port.addShip(titanic);
-    port.addShip(mary);
-    port.removeShip(mary);
-
-    expect(port.ships).toEqual([titanic]);
-  });
-  it('gets added to the port on instantiation', () => {
-    const poopyville = new Port('Poopyville');
-    const itinerary = new Itinerary([poopyville]);
-    const ship = new Ship(itinerary);
-
-    expect(poopyville.ships).toContain(ship);
+      expect(fartington.ships).toEqual([titanic]);
+    });
   });
 });
